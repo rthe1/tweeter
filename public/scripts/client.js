54a061c984330a37b0e72c39bcb1179978e44dc2
Reshawn <reshawnhamilton97@gmail.com>
2021-12-20 07:23:42 +0000

    updated error message

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,18 +6,52 @@
 
 
 $(document).ready(function () {
+  
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
 
   $('#tweet-form').submit(function (event) {
 
     event.preventDefault();
+
+    let textArea = $('#tweet-text');
+
+    let textFromUser = textArea.val();
+        
+    $("#tweet-text").text(textFromUser);
+
+    if(textArea.val() === null || textArea.val() === ''){
+      let emptyError = `<div>PLEASE ENTER A TWEET</div>`
+      $(".error-message").append(emptyError)
+      
+      setTimeout(function(){
+        $(".error-message").empty();
+      },1000)
+    };
+
+    if(textArea.val().length > 140){
+      let fullError = `<div>TOO MANY WORDS</div>`
+      $(".error-message").append(fullError)
+
+      setTimeout(function(){
+        $(".error-message").empty();
+      },1000)
+
+    };
+
+    // alert(textFromUser);
 
     $.ajax({
       url: '/tweets',
       method: 'POST',
       data: $(this).serialize(),
       success: () => {
+        
         $(this)[0].reset();
-
         loadTweets();
 
       },
@@ -30,12 +64,11 @@ $(document).ready(function () {
 
   })
 
-
   const renderTweets = function (tweets) {
 
     $('#tweet').empty();
     tweets.forEach(function (tweet) {
-      $('#tweet').append(createTweetElement(tweet))
+      $('#tweet').prepend(createTweetElement(tweet))
     })
   }
 
@@ -45,23 +78,23 @@ $(document).ready(function () {
 <div class="tweet-con-top">
     <div class="tweet-con-top-left">
       <img width="50px" height="50px"
-        src=${tweet.user.avatars}>
-      <h5 class="username">${tweet.user.name}</h5>
+        src=${escape(tweet.user.avatars)}>
+      <h5 class="username">${escape(tweet.user.name)}</h5>
     </div>
     <!--space between-->
-    <h5 class="handle">${tweet.user.handle}</h5>
+    <h5 class="handle">${escape(tweet.user.handle)}</h5>
   </div>
   <!--Top Tweet Con END-->
 
 
   <div class="tweet-con-mid">
-    <h4>${tweet.content.text}</h4>
+    <h4>${escape(tweet.content.text)}</h4>
   </div>
 
 
   <div class="tweet-con-bottom">
     <div class="tweet-con-bottom-left">
-      <h6 class="time-ago">${timeago.format(tweet.created_at)}</h6>
+      <h6 class="time-ago">${escape(timeago.format(tweet.created_at))}</h6>
     </div>
     <!--space between-->
     <div class="tweet-con-bottom-right">
@@ -76,7 +109,6 @@ $(document).ready(function () {
     return $tweet;
   }
 
-
   const loadTweets = function () {
 
     $.ajax({
@@ -89,5 +121,4 @@ $(document).ready(function () {
 
   }
   loadTweets();
-
 })
